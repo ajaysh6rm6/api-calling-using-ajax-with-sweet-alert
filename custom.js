5874ecd33f8dc18de597169ec4f2b542d498e95d
TECHpy <90966200+ajaysh6rm6@users.noreply.github.com>
2022-05-19 12:59:56 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,68 +1,63 @@
-(function ($) {
-
-  ///////////////contact_us//////////////////
-  // Get value on button click and show sweet alert
-  $("#contact_us").click(function () {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    var fullname = $("input#fullname").val();
-    var email = $("input#email").val();
-    var subject = $("input#subject").val();
-    var msg = $("textarea#msg").val();
-    if (fullname == "") {
-      swal("Error !", "Please enter your full name ?", "error");
-    } else if (email == "") {
-      swal("Error !", "Please enter your email ?", "error");
-    } else if (!regex.test(email)) {
-      swal("Error !", "Please enter valid email address ?", "error");
-    } else if (subject == "") {
-      swal("Error !", "Please enter your subject ?", "error");
-    } else if (msg == "") {
-      swal("Error !", "Please enter your msg ?", "error");
-    } else {
-      var settings = {
-        "url": "https://............",
-        "method": "POST",
-        "headers": {
-          "Content-Type": "application/json"
-        },
-        "data": JSON.stringify({
-          "name": fullname,
-          "email": email,
-          "subject": subject,
-          "message": msg
-        }),
-      };
-      swal({
-        title: "Are you sure ?",
-        text: "You want to submit a request !",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes, confirm it !",
-        closeOnConfirm: false
-      }, function (isConfirm) {
-        if (!isConfirm) return;
-        $.ajax({
-          ...settings,
-          success: function (data) {
-            swal("Done !", "Your request submitted succesfully !", "success");
-            $("input#fullname").val("");
-            $("input#email").val("");
-            $("input#subject").val("");
-            $("textarea#msg").val("");
-          },
-          error: function (xhr, ajaxOptions, thrownError) {
-            $("input#fullname").val("");
-            $("input#email").val("");
-            $("input#subject").val("");
-            $("textarea#msg").val("");
-            swal("Error !", "Please try again, if urgent please call me !", "error");
-          }
-        });
-      });
-    }
-  });
-  ///////////////contact_us//////////////////
-
-
-})(jQuery); // End of use strict
+$(document).ready(function () {
+    $("#index_quote_submit").click(function (event) {
+        event.preventDefault();
+        var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        var YourName = $("input#YourName").val();
+        var YourEmail = $("input#YourEmail").val();
+        var SelectAService = $("select#SelectAService").val();
+        var Msg = $("textarea#Msg").val();
+        if (YourName == "") {
+            swal("Warning !", "Please enter your name ?", "warning");
+        } else if (YourEmail == "") {
+            swal("Warning !", "Please enter your email ?", "warning");
+        } else if (!email_regex.test(YourEmail)) {
+            swal("Warning !", "Please enter valid email address ?", "warning");
+        } else if (SelectAService == "") {
+            swal("Warning !", "Please select a service ?", "warning");
+        } else if (Msg == "") {
+            swal("Warning !", "Please enter your msg ?", "warning");
+        } else {
+            var settings = {
+                "url": "https://............",
+                "method": "POST",
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "data": JSON.stringify({
+                    "name": YourName,
+                    "email": YourEmail,
+                    "service": SelectAService,
+                    "message": Msg
+                }),
+            };
+            swal({
+                title: "Are you sure ?",
+                text: "You want to submit a request !",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, confirm it !",
+                closeOnConfirm: false
+            }, function (isConfirm) {
+                if (!isConfirm) return;
+                $.ajax({
+                    ...settings,
+                    success: function (data) {
+                        swal("Done !", "Your request submitted succesfully !", "success");
+                        $("input#YourName").val("");
+                        $("input#YourEmail").val("");
+                        $("select#SelectAService").val("");
+                        $("textarea#Msg").val("");
+                    },
+                    error: function (xhr, ajaxOptions, thrownError) {
+                        $("input#YourName").val("");
+                        $("input#YourEmail").val("");
+                        $("select#SelectAService").val("");
+                        $("textarea#Msg").val("");
+                        swal("Error !", "Please try again, if urgent please call us !", "error");
+                    }
+                });
+            });
+        }
+    });
+});
